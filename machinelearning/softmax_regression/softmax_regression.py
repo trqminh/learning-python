fix softmax, add .gitignore

machinelearning/softmax_regression/softmax_regression.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import scipy.special
 
 
-def compute_logits(X, W):
+def compute_logits(X, W, biases):
   # sofmax function
-  hX = X.dot(W)
+  hX = X.dot(W) + biases
   hX = np.exp(hX - np.max(hX, axis=1, keepdims=True))
   return hX / ((np.sum(hX, axis=1)).reshape(-1, 1))
 
@@ -13,17 +13,17 @@ def compute_logits(X, W):
 def softmax_regression(X, y, learning_rate):
   #init weight
   W = np.random.randn(X.shape[1], y.shape[1])
+  biases = np.ones(shape=[X.shape[0], y.shape[1]], dtype=np.float32)
 
   # batch gradient descent
   it = 10000
   while it:
     it -= 1
-    logits = compute_logits(X, W)
+    logits = compute_logits(X, W, biases)
     W -= learning_rate*X.transpose().dot(logits - y)
+    biases -= learning_rate*(logits - y)
 
-
-
-  return W
+  return W, biases
 
 
 def main():
@@ -51,18 +51,28 @@ def main():
 
 
   learning_rate = 0.1
-  W = softmax_regression(X, y, learning_rate)
+  W, biases = softmax_regression(X, y, learning_rate)
 
   x_w = np.linspace(0, 10, 5000)
-  y_w_1 = (W[0][0] - W[0][1]) * x_w / (W[1][1] - W[1][0])
+  y_w = ((W[0][1] - W[0][0])*x_w + (biases[0][1] - biases[0][0])) / (W[1][0] - W[1][1])
+
+  x_w1 = np.linspace(0, 10, 5000)
+  y_w1 = ((W[0][0] - W[0][2])*x_w + (biases[0][0] - biases[0][2])) / (W[1][2] - W[1][0])
+
+  x_w2 = np.linspace(0, 10, 5000)
+  y_w2 = ((W[0][2] - W[0][1])*x_w + (biases[0][2] - biases[0][1])) / (W[1][1] - W[1][2])
 
   print(W)
+
+
 
   # plot data and result
 
   plt.scatter(X[:,0], X[:,1])
-  plt.plot(x_w, y_w_1)
-  #plt.plot(x_w, y_w_2)
+  # plt.plot(x_w, y_w)
+  # plt.plot(x_w1,y_w1)
+  plt.plot(x_w2,y_w2)
+
 
   plt.show()
 