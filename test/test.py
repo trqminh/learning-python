3a711dd6ad8cb0f0b65a0bec3a7ea6ce993e1a3a
1612374 <1612374@student.hcmus.edu.vn>
2019-07-17 00:31:17 +0700

    add some pytorch

--- test/test.py
+++ test/test.py
@@ -1,44 +1,7 @@
-n, m = list(map(int, input().split(' ')))
+import torch
 
-mat = [None for i in range(n)]
-
-for i in range(n):
-    mat[i] = list(map(int, input().split(' ')))
-
-
-tmp_xor = 0
-for i in range(n):
-    tmp_xor ^= mat[i][0]
-
-
-if tmp_xor:
-    print('TAK')
-    for i in range(n):
-        print('1 ', end = '')
-else:
-    flag = False
-    idx = 0
-    val = 0
-    for i in range(n):
-        for j in range(m):
-            if mat[i][j] != mat[i][0]:
-                flag = True
-                idx = i
-                val = j
-                break
-
-        if flag:
-            break
-
-    if flag:
-        print('TAK')
-        for i in range(idx):
-            print('1', end = ' ')
-
-        print(val+1, end = ' ')
-
-        for i in range(idx+1, n, 1):
-            print('1', end = ' ')
-    else:
-        print('NIE')
-
+x = torch.randn(3, 1, requires_grad=True)
+y = x * 2
+v = torch.tensor([[1.], [1.], [1.]], dtype=torch.float)
+y.backward(v)
+print(x.grad)
